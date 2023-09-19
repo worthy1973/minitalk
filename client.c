de6fdd340bc922cc8c58fe12dc81a8dac0204d3f
Daniel López <35295016+worthy1973@users.noreply.github.com>
2023-09-19 15:27:29 +0200

    Client ok

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: dlopez-i <dlopez-i@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 11:02:02 by dlopez-i          #+#    #+#             */
-/*   Updated: 2023/09/19 14:52:37 by dlopez-i         ###   ########.fr       */
+/*   Updated: 2023/09/19 15:08:06 by dlopez-i         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,34 @@ void   turn_bin(const int pid)
         else
             send_signal(pid, SIGUSR2);
         bite--;
-        if (usleep(200)) == ERROR)
+        if (usleep(200) == ERROR)
             exit (error_msg(E_US, EXIT_FAILURE));
     }   
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	s;
+	int	r;
+
+	i = 0;
+	s = 1;
+	r = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			s = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		r = (r * 10) + (str[i] - '0');
+			i++;
+	}
+	return (r * s);
 }
 
 int main(int ac, char **av)
