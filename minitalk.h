3a32b66b93decf8f7a27ac61b2b75f2f140de0ef
Daniel López <35295016+worthy1973@users.noreply.github.com>
2023-09-19 15:11:30 +0200

    Create minitalk.h

---
+++ minitalk.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlopez-i <dlopez-i@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 14:59:51 by dlopez-i          #+#    #+#             */
+/*   Updated: 2023/09/19 15:00:32 by dlopez-i         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <stdio.h>
+# include <signal.h>
+# include <limits.h>
+# include <sys/types.h>
+# include "../libft/libft.h"
+# include "../libft/ft_printf.h"
+
+# define SUCCESS 0
+# define FALSE   0
+# define TRUE    1
+# define ERROR  -1
+# define E_EXIT  1
+
+# define E_FA 1
+# define E_MA 2
+# define E_KL 3
+# define E_US 4
+
+#endif
