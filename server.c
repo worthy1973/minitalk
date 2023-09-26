68c95d781c678a4ba054d1a3eb08f9a5c34ba2cb
worthy1973 <daniel.lopez@personasfelices.com>
2023-09-26 12:53:01 +0200

    Work in progress

--- server.c
+++ server.c
@@ -3,10 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dani <dani@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: dlopez-i <dlopez-i@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/26 11:13:35 by dani              #+#    #+#             */
-/*   Updated: 2023/09/26 11:13:54 by dani             ###   ########.fr       */
+/*   Created: 2023/09/26 11:13:35 by dlopez-i          #+#    #+#             */
+/*   Updated: 2023/09/26 12:53:01 by dlopez-i         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minitalk.h"
+
+void    ft_handler(int sig)
+{
+    static int		bite = 0;
+    static unsigned char		ch = '\0';
+
+    ch <<= 1;
+    if (sig == SIGUR1))
+        ch |= 1;
+    bite++;
